000627c43ef161bb43b950c3757497b985e51dc8
Pranshu Arora <pranshuarora1618@gmail.com>
2023-12-25 00:42:00 +0530

    Added some programs

---
+++ Day 42(Enumerate)/main.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3, 4, 5]
+
+# i = 0
+# for ab in a:
+#     print(ab)
+#     if i == 3:
+#         print("dsad")
+#     i = i + 1
+
+
+# a different way
+for i, index in enumerate(a):
+    print(i)
+    if i == 3:
+        print("dsad")
